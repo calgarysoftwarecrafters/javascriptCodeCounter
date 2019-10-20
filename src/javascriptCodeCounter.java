cf9c0e22ae1180fb43074b5bc87b7aa0ff469392
venetrius <gergelygjuhasz@gmail.com>
2019-10-20 15:57:08 -0600

    slice text to lines and iterate over it

--- src/javascriptCodeCounter.java
+++ src/javascriptCodeCounter.java
@@ -4,6 +4,10 @@ public class javascriptCodeCounter {
     }
 
     public static int countCodeLines(String javascriptFile){
+        String lines[] = javascriptFile.split("\\r?\\n");
+        for(int i = 0; i < lines.length; i++){
+          System.out.println(lines[i]);
+        }
         return -1;
     }
 }
