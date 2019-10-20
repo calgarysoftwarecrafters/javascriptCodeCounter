8e5b06c067297679b91b041952a4ec0d9b45e7e4
venetrius <gergelygjuhasz@gmail.com>
2019-10-20 16:05:16 -0600

    transforming into member function

--- src/javascriptCodeCounter.java
+++ src/javascriptCodeCounter.java
@@ -23,16 +23,11 @@ public class javascriptCodeCounter {
         return true;
     }
 
-    /* A mock function to call from countCodeLines */
-    public static boolean mockIsCodeLine(String line){
-      return false;
-    }
-
-    public static int countCodeLines(String javascriptFile){
+    public int countCodeLines(String javascriptFile){
         int count = 0;
         String lines[] = javascriptFile.split("\\r?\\n");
         for(int i = 0; i < lines.length; i++){
-          if(mockIsCodeLine(lines[i])){
+          if(shouldBeCounted(lines[i])){
             count++;
           }
         }
