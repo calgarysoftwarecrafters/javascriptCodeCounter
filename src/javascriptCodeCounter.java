e6a8a6dd877f637065b0eeb632e5e4164b8b5b52
venetrius <gergelygjuhasz@gmail.com>
2019-10-20 15:55:35 -0600

    add function to accept text file and count lines

--- src/javascriptCodeCounter.java
+++ src/javascriptCodeCounter.java
@@ -2,4 +2,8 @@ public class javascriptCodeCounter {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static int countCodeLines(String javascriptFile){
+        return -1;
+    }
 }
