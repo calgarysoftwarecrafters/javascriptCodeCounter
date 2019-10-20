c0e085cd8126cc8b9173f60a4e5301a263c7f28c
venetrius <gergelygjuhasz@gmail.com>
2019-10-20 16:02:29 -0600

    add mockIsCodeLine and call it from countCodeLines

--- src/javascriptCodeCounter.java
+++ src/javascriptCodeCounter.java
@@ -3,11 +3,19 @@ public class javascriptCodeCounter {
         System.out.println("Hello World");
     }
 
+    /* A mock function to call from countCodeLines */
+    public static boolean mockIsCodeLine(String line){
+      return false;
+    }
+
     public static int countCodeLines(String javascriptFile){
+        int count = 0;
         String lines[] = javascriptFile.split("\\r?\\n");
         for(int i = 0; i < lines.length; i++){
-          System.out.println(lines[i]);
+          if(mockIsCodeLine(lines[i])){
+            count++;
+          }
         }
-        return -1;
+        return count;
     }
 }
