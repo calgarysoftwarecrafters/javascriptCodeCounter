60342d3167424ebe6803e68c25753b64c07c00a3
Ian Graham <igraham@iname.com>
2019-10-20 20:59:28 -0600

    Converted new tests to runnable JUnit5 tests

--- src/SourceLineCounterTests.java
+++ src/SourceLineCounterTests.java
@@ -1,24 +1,43 @@
-package kata.tests;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SourceLineCounterTests {
 
+	private javascriptCodeCounter counter;
+
 	@BeforeEach
 	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+		counter = new javascriptCodeCounter();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void emptyFileShouldHaveZeroLines() {
+		assertEquals(0, counter.countCodeLines(""));
 	}
 
+	@Test
+	void newlineOnlyFileShouldHaveZeroLines() {
+		assertEquals(0, counter.countCodeLines("\n"));
+	}
+
+	@Test
+	void spaceOnlyFileShouldHaveZeroLines() {
+		assertEquals(0, counter.countCodeLines(" \t\n"));
+	}
+	
+	@Test
+	void simpleJavascriptWithNoCommentOnlyLines() {
+		String script = "var x, y, z;  // Declare 3 variables\n" + 
+				"x = 5;    // Assign the value 5 to x\n" + 
+				"y = 6;    // Assign the value 6 to y\n" + 
+				"z = x + y;  // Assign the sum of x and y to z\n" + 
+				"\n" + 
+				"document.getElementById(\"demo\").innerHTML =\n" + 
+				"\"The value of z is \" + z + \".\";\n" + 
+				"";
+		int lineCount = counter.countCodeLines(script);
+		assertEquals(6, lineCount, "");
+	}
 }
